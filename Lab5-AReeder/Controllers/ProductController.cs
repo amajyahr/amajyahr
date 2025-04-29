0a38b948141ae84cf91aee18d6da2d70b8014175
Amajyah Reeder <amajyahr@gmail.com>
2025-04-29 17:24:53 -0400

    Web API

---
+++ Lab5-AReeder/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab5_AReeder.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab5_AReeder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.99m },
+            new Product { Id = 2, Name = "Product 2", Price = 20.99m },
+            new Product { Id = 3, Name = "Product 3", Price = 30.99m }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Product> UpdateProduct(int id, Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return NoContent();
+
+        }
+    }
+}
---
+++ Lab5-AReeder/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5_AReeder.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
